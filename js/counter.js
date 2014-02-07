0909a1491bc505e346dc82a32441d909ecd7cb0e
Roman Zadov <roman@oatbit.com>
2014-02-06 23:55:04 -0800

    added decimals, real salaries

--- js/counter.js
+++ js/counter.js
@@ -1,10 +1,10 @@
 $(function(){
-	makeCounter("Firefighter", 30000);
-	makeCounter("Teacher", 50000);
-	makeCounter("Nurse Practicioner", 80000);
-	makeCounter("Lawyer", 200000);
-	makeCounter("CEO", 10000000);
-	makeCounter("Hedge Fund Manager", 3000000000); 
+	makeCounter("Median US household", 45000);
+	makeCounter("Teacher", 55000);
+	makeCounter("Nurse Practicioner", 95000);
+	makeCounter("Lawyer", 130000);
+	makeCounter("CEO", 950000);
+	makeCounter("Hedge Fund Manager", 700000000); 
 });
 
 function makeCounter(profession, salary) {
@@ -13,10 +13,17 @@ function makeCounter(profession, salary) {
 	
 	var counters = $("#counters");
 	var counter = $("<div>").addClass("counter").addClass(profession);
-	for (var i = 0; i<10; i++) {
+	for (var i = 0; i<11; i++) {
 		var digit = $("<div>").addClass("number");
 		digit.data("digit", i);
 		counter.append(digit);
+		if (i == 8) {
+			digit.addClass("last")
+			counter.append($("<div>").addClass("decimal").text("."));
+		}
+		if (i == 10) {
+			digit.addClass("last")
+		}
 	}
 	var counterAndSymbol = $("<div>").addClass("counterAndSymbol").append($("<div>").addClass("dollar").text("$")).append(counter);
 	counters.append(counterAndSymbol);
@@ -28,10 +35,10 @@ function makeCounter(profession, salary) {
 function getDigitValue(currentSalary, rollerNumber) {
 	var salaryString = String(currentSalary).split("").reverse().join("");
 	
-	if (10-salaryString.length <= rollerNumber) {
-		return salaryString.charAt(9-rollerNumber);
+	if (11-salaryString.length <= rollerNumber) {
+		return salaryString.charAt(10-rollerNumber);
 	}
-	return 0;
+	return "-";
 }
 
 function updateSalaryInCounter(counter, totalSalary) {
@@ -40,11 +47,12 @@ function updateSalaryInCounter(counter, totalSalary) {
 	for (var i = 0; i<numbers.length; i++) {
 		var number = $(numbers[i]);
 		number.text(getDigitValue(currentSalary, number.data("digit")));
+		number.toggleClass("dash", number.text() == "-");
 	}
 }
 
 function getCurrentSalary(totalSalary) {
 	var currentTime = new Date().getTime();
-	return Math.ceil(totalSalary * ((currentTime - 1388534400000)/31557600000))
+	return Math.ceil(totalSalary * ((currentTime - 1388534400000)/31557600000*100))
 }
 
