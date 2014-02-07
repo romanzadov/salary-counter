bb93cde1ef1754d738b8d60c141d5dca9d063dc1
Roman Zadov <roman@oatbit.com>
2014-02-07 00:24:40 -0800

    added inputs for custom counters

--- js/counter.js
+++ js/counter.js
@@ -5,6 +5,22 @@ $(function(){
 	makeCounter("Lawyer", 130000);
 	makeCounter("CEO", 950000);
 	makeCounter("Hedge Fund Manager", 700000000); 
+	
+	var amount = $("#counters").find("input");
+	amount.on("keyup", function(){
+		var valid = /^\d{0,11}(\.\d{0,2})?$/.test(this.value),
+			val = this.value;
+		if(!valid){
+			this.value = val.substring(0, val.length - 1);
+		}
+	});
+	$("#add-link").on("click", function(){
+		var salary = amount.val();
+		var name = $("#professions").find("input").val();
+		if (name != "") {
+			makeCounter(name, salary);
+		}
+	});
 });
 
 function makeCounter(profession, salary) {
